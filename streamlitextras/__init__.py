13f6dc8bbdea020031ef35a286341ff3f0dd454f
vsol <45532845+blipk@users.noreply.github.com>
2023-07-21 17:11:48 +1000

    remove universal imports to prevent conflicts

--- streamlitextras/__init__.py
+++ streamlitextras/__init__.py
@@ -1,7 +1,7 @@
-from .router import Router
-from .cookiemanager import CookieManager
-from .authenticator import Authenticator
-from .authenticator.user import User
-from .authenticator.exceptions import AuthException, LoginError, RegisterError, ResetError, UpdateError
+# from .router import Router
+# from .cookiemanager import CookieManager
+# from .authenticator import Authenticator
+# from .authenticator.user import User
+# from .authenticator.exceptions import AuthException, LoginError, RegisterError, ResetError, UpdateError
 
-__version__ = "0.2.26"
+__version__ = "0.2.27"
